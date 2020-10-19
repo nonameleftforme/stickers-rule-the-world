410a9b4ffb6882e16602327a103ead9f63e665ae
nonameleftforme <taty.vogel@gmx.ch>
2020-10-19 16:02:12 +0200

    Add header tag with meta info

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Upload from '../components/Upload'
+import Head from 'next/head'
 
 export default function IndexPage() {
   return (
     <div>
+      <Head>
+        <title>Stickers rule the world</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charset="utf-8" />
+      </Head>
       <Upload />
     </div>
   )
